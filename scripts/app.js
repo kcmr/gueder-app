205f5d780b4b20ca439bedc3b426567fc69efcac
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-04 23:10:15 +0200

    feat(app): get geolocation

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,26 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./service-worker.js')
-    .then(() => {
-      console.log('Service Worker registered');
-    });
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('./service-worker.js')
+//     .then(() => {
+//       console.log('Service Worker registered');
+//     });
+// }
+
+function getGeolocation() {
+  return new Promise((resolve, reject) => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        resolve({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        });
+      }, err => reject(err));
+    } else {
+      throw new Error('Your browser does not support geolocation :(');
+    }
+  });
 }
+
+getGeolocation()
+  .then(res => console.log(JSON.stringify(res, null, 2)))
+  .catch(err => console.log(err.message));
