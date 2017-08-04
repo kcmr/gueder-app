6a6a7388edf80cb52e79f468c000761eecd1e79e
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-05 01:07:36 +0200

    feat(*): get weather from location

--- scripts/app.js
+++ scripts/app.js
@@ -21,6 +21,33 @@ function getGeolocation() {
   });
 }
 
+function getWeather(lat, lng) {
+  const API_KEY = '7e1fd95cfe31530bc20639de15507835';
+  let weatherUrl = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lng}?language=es&units=auto&callback=getWeather.parseData`;
+  let script = document.createElement('script');
+  script.src = weatherUrl;
+  document.getElementsByTagName('head')[0].appendChild(script);
+
+  return new Promise((resolve, reject) => {
+    getWeather.parseData = data => {
+      resolve(JSON.parse(JSON.stringify(data)));
+    };
+
+    script.onerror = err => reject(err);
+  });
+}
+
+function setText(element, text) {
+  document.querySelector(element).innerText = text;
+}
+
 getGeolocation()
-  .then(res => console.log(JSON.stringify(res, null, 2)))
+  .then(location => {
+    setText('#loadingMessage', 'Getting weather');
+    return getWeather(location.lat, location.lng);
+  })
+  .then(weather => {
+    console.log(weather.currently);
+    setText('#loadingMessage', `Temperature: ${Math.round(weather.currently.temperature)} °C`);
+  })
   .catch(err => console.log(err.message));
