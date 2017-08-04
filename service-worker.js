f79b53fa5be4956ed1d400ddc3e4d2f9aa4b9dc2
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-04 22:35:43 +0200

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const cacheName = 'gueder-app-1';
+const filesToCache = [];
+
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+  );
+});
