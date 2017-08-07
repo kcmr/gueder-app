4e0751376ecae7726283a74c7f1570c82b70c4b0
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-07 18:13:49 +0200

    refactor(*): use vue

--- scripts/app.js
+++ scripts/app.js
@@ -1,63 +1,51 @@
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('./service-worker.js')
-//     .then(() => {
-//       console.log('Service Worker registered');
-//     });
-// }
-
-const GuederApp = {
-  API_KEY: '7e1fd95cfe31530bc20639de15507835',
-  weatherEndpoint: 'https://api.darksky.net/forecast/',
-  language: 'es',
-
-  getGeolocation() {
-    return new Promise((resolve, reject) => {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(pos => {
-          resolve({
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude
-          });
-        }, err => reject(err));
-      } else {
-        throw new Error('Your browser does not support geolocation :(');
-      }
-    });
+const GeoApp = new Vue({
+  el: '#app',
+  data: {
+    API_KEY: '7e1fd95cfe31530bc20639de15507835',
+    weatherEndpoint: 'https://api.darksky.net/forecast/',
+    language: 'es',
+    message: 'Finding you...'
   },
-
-  getWeather({lat, lng}) {
-    let weatherUrl = `${this.weatherEndpoint}${this.API_KEY}/${lat},${lng}?language=${this.language}&units=auto&callback=GuederApp.getWeather.parseData`;
-    let script = document.createElement('script');
-    script.src = weatherUrl;
-    document.getElementsByTagName('head')[0].appendChild(script);
-
-    return new Promise((resolve, reject) => {
-      this.getWeather.parseData = data => {
-        resolve(JSON.parse(JSON.stringify(data)));
-      };
-
-      script.onerror = err => reject(err);
-    });
+  methods: {
+    getGeolocation: function() {
+      return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition(pos => {
+            resolve({
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude
+            });
+          }, err => reject(err));
+        } else {
+          throw new Error('Your browser does not support geolocation :(');
+        }
+      });
+    },
+    getWeather: function({lat, lng}) {
+      let weatherUrl = `${this.weatherEndpoint}${this.API_KEY}/${lat},${lng}?language=${this.language}&units=auto&callback=GeoApp.getWeather.parseData`;
+      let script = document.createElement('script');
+      script.src = weatherUrl;
+      document.getElementsByTagName('head')[0].appendChild(script);
+  
+      return new Promise((resolve, reject) => {
+        this.getWeather.parseData = data => {
+          resolve(JSON.parse(JSON.stringify(data)));
+        };
+  
+        script.onerror = err => reject(err);
+      });
+    }    
   },
-
-  setText(element, text) {
-    document.querySelector(element).innerText = text;
-  },
-
-  init() {
+  created: function() {
     this.getGeolocation()
       .then(location => {
-        this.setText('#loadingMessage', 'Getting weather');
+        this.message = 'Getting weather';
         return this.getWeather(location);
       })
       .then(weather => {
         console.log(weather.currently);
-        this.setText('#loadingMessage', `Temperature: ${Math.round(weather.currently.temperature)} °C`);
+        this.message = `Temperature: ${Math.round(weather.currently.temperature)} °C`;
       })
       .catch(err => console.log(err.message));
   }
-};
-
-GuederApp.init();
-
+});
